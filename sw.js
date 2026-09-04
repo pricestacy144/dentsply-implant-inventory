1d16f13ebe32c5734235bc130b29152544d0ea8e
pricestacy144 <pricestacy144@gmail.com>
2026-09-03 20:27:50 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "implant-inventory-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => cached))
+  );
+});
